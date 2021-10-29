a7b09b521605e5b88e4a1abb946075f7c8f3b6f1
Woyneshet Bilihatu <58231550+woynshetb@users.noreply.github.com>
2021-10-29 09:23:22 +0300

    Add files via upload

--- lib/Admin/adminLogin.dart
+++ lib/Admin/adminLogin.dart
@@ -1,1 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'Admin/uploadItems.dart';
+import 'Authentication/authenication.dart';
+import 'Widgets/customTextField.dart';
+import 'DialogBox/errorDialog.dart';
+import 'package:flutter/material.dart';
 
+class AdminSignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class AdminSignInScreen extends StatefulWidget {
+  @override
+  _AdminSignInScreenState createState() => _AdminSignInScreenState();
+}
+
+class _AdminSignInScreenState extends State<AdminSignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView();
+  }
+}
---
+++ lib/Admin/adminOrderDetails.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'Admin/uploadItems.dart';
+import 'Config/config.dart';
+import 'Widgets/loadingWidget.dart';
+import 'Widgets/orderCard.dart';
+import 'Models/address.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class AdminOrderDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea();
+  }
+}
+
+class StatusBanner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class PaymentDetailsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ShippingDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
+
+class KeyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
