86790a396120c2d63e9ef05eef2e3a42592483a8
Woyneshet Bilihatu <58231550+woynshetb@users.noreply.github.com>
2021-10-29 09:45:36 +0300

    3 dart files are created

---
+++ lib/Orders/placeOrder.dart
@@ -0,0 +1,18 @@
+import 'Config/config.dart';
+import 'Store/storehome.dart';
+import 'Counters/cartitemcounter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
