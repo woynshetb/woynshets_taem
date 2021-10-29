eff3a3f8372084fd5c9765e2a6a3503a9ec67b89
Woyneshet Bilihatu <58231550+woynshetb@users.noreply.github.com>
2021-10-29 09:57:44 +0300

    Add files via upload

---
+++ lib/Widgets/customAppBar.dart
@@ -0,0 +1,16 @@
+import 'Store/cart.dart';
+import 'Counters/cartitemcounter.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyAppBar extends StatelessWidget with PreferredSizeWidget {
+  final PreferredSizeWidget bottom;
+  MyAppBar({this.bottom});
+
+  @override
+  Widget build(BuildContext context) {}
+
+  Size get preferredSize => bottom == null
+      ? Size(56, AppBar().preferredSize.height)
+      : Size(56, 80 + AppBar().preferredSize.height);
+}
