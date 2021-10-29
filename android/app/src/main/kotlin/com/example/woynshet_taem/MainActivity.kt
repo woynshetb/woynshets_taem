0f22b39ec52a1d8de239cebad6e3099249a95b8b
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-10-29 03:27:26 +0300

    empty flutter project is created

---
+++ android/app/src/main/kotlin/com/example/woynshet_taem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.woynshet_taem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child:Scaffold(
+        body:Container(
+  child:Text("hey"),
+      
+    )
+      )
+    );
+  }
+}
