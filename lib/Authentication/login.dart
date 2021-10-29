fee40f9cf45657bc59a79f46553baa160fefa4f4
Woyneshet Bilihatu <58231550+woynshetb@users.noreply.github.com>
2021-10-29 09:27:45 +0300

    Add files via upload

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -1,1 +1,21 @@
+import 'Admin/adminLogin.dart';
+import 'Widgets/customTextField.dart';
+import 'DialogBox/errorDialog.dart';
+import 'DialogBox/loadingDialog.dart';
+import 'Config/config.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+import 'package:flutter/material.dart';
+import '../Store/storehome.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
