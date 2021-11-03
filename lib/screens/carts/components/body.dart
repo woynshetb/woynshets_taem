af9d228e634036d1c78964f852265d35bf5773ea
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-11-03 17:46:45 +0300

    cart ui code is deleted b/c it was added for only for demo purpose the code was hardcoded

--- lib/screens/carts/components/body.dart
+++ lib/screens/carts/components/body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:woynshet_taem/constants.dart';
+
 import 'package:woynshet_taem/screens/carts/components/cart_item_card.dart';
-import 'package:woynshet_taem/size_config.dart';
 
 var id = 5;
 
@@ -13,80 +12,7 @@ class Body extends StatelessWidget {
         Padding(
             padding: EdgeInsets.symmetric(vertical: 10),
             child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFE6E6),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CartitemCard(
-                        itemNumber: 4,
-                        img: "assets/images/mitmita.jpg",
-                        price: 34,
-                        text: "chil is  heglo world is written by me",
-                      ),
-                      Icon(Icons.delete),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFE6E6),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CartitemCard(
-                        itemNumber: 4,
-                        img: "assets/images/mitmita.jpg",
-                        price: 34,
-                        text: "chil is  heglo world is written by me",
-                      ),
-                      Icon(Icons.delete),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFE6E6),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CartitemCard(
-                        itemNumber: 4,
-                        img: "assets/images/mitmita.jpg",
-                        price: 34,
-                        text: "chil is  heglo world is written by me",
-                      ),
-                      Icon(Icons.delete),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFE6E6),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CartitemCard(
-                        itemNumber: 4,
-                        img: "assets/images/mitmita.jpg",
-                        price: 34,
-                        text: "chil is  heglo world is written by me",
-                      ),
-                      Icon(Icons.delete),
-                    ],
-                  ),
-                ),
-              ],
+              children: [],
             )),
       ],
     );
