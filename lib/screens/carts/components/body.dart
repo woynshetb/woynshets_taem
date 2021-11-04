84b09f101c2b81ea69c0b798ca9b4a93e5630787
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-11-04 08:15:33 +0300

    edit splash router name

--- lib/screens/carts/components/body.dart
+++ lib/screens/carts/components/body.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:woynshet_taem/screens/carts/components/cart_item_card.dart';
-
-var id = 5;
-
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -4,7 +4,7 @@ import 'package:woynshet_taem/screens/splash/components/body.dart';
 import 'package:woynshet_taem/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
-  static String routeName = "/spllash";
+  static String routeName = "/splash";
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
