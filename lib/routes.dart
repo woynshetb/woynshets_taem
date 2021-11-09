95f36b1a3d107ac3bad220748fef2129e2e1152c
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-11-09 23:43:35 +0300

    deleted otp folders and files b/c otp is not currently need for this project

--- lib/routes.dart
+++ lib/routes.dart
@@ -6,7 +6,7 @@ import 'package:woynshet_taem/screens/details/details-screen.dart';
 import 'package:woynshet_taem/screens/forgot_password/forgot_password.dart';
 import 'package:woynshet_taem/screens/home/home-screen.dart';
 import 'package:woynshet_taem/screens/login_success/login_success.dart';
-import 'package:woynshet_taem/screens/otp/otp_screen.dart';
+
 import 'package:woynshet_taem/screens/sign_in/sign_in_screen.dart';
 import 'package:woynshet_taem/screens/sign_up/sign_up_screen.dart';
 import 'package:woynshet_taem/screens/splash/splash.dart';
@@ -18,7 +18,6 @@ final Map<String, WidgetBuilder> routes = {
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   SignUpScreen.routeName: (context) => SignUpScreen(),
   CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
-  OTPScreen.routName: (context) => OTPScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
   CartScreen.routeName: (context) => CartScreen(),
