72d1a47c0b827e81e7389fda5e59c9a893de3075
Yared Getachew <yared.getachew.tessema@gmail.com>
2021-11-11 13:57:33 +0300

    product renamed

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import '../models/products.dart';
+import '../models/product.dart';
 import 'dart:core';
 
 class ProductProvider {
@@ -11,21 +11,41 @@ class ProductProvider {
   static Future<List<Product>> fetchProducts() async {
     List<Product> products = [];
     final response = await http.get(Uri.parse(BASE_URL + '/products'));
-    print(response);
+    // print(response);
     if (response.statusCode == 200) {
       final decoded = await json.decode(response.body);
       print(decoded);
       decoded.forEach((p) {
         print(p);
         var product = new Product(
+            id: p["id"],
             title: p["title"],
             price: p["price"],
+            category: p["category"],
             description: p["description"],
             image_url: p["image"]);
         products.add(product);
       });
 
       return products;
+    } else {
+      throw Exception("An exception occurred");
+    }
+  }
+
+  static Future<Product> findProduct(int id) async {
+    final response =
+        await http.get(Uri.parse(BASE_URL + '/products/' + id.toString()));
+    if (response.statusCode == 200) {
+      final decoded = await json.decode(response.body);
+      Product product = new Product(
+          id: decoded["id"],
+          title: decoded["title"],
+          price: decoded["price"],
+          category: decoded["category"],
+          description: decoded["description"],
+          image_url: decoded["image"]);
+      return product;
     } else {
       throw Exception("An exception occurred");
     }
