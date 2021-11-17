bd95408b3266cd2ab6df15674a51bdd33897fa47
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-11-17 12:33:25 +0300

    added fetch from cart functionality to the app

--- lib/screens/carts/cart_screen.dart
+++ lib/screens/carts/cart_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:woynshet_taem/components/default_button.dart';
-import 'package:woynshet_taem/constants.dart';
+
 import 'package:woynshet_taem/screens/carts/components/body.dart';
-import 'package:woynshet_taem/size_config.dart';
 
 class CartScreen extends StatelessWidget {
   static String routeName = "/cart";
@@ -17,7 +15,6 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context),
       body: Body(
         title: title,
         productId: productId,
@@ -25,21 +22,4 @@ class CartScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-AppBar buildAppBar(BuildContext context) {
-  return AppBar(
-    title: Column(
-      children: [
-        Text(
-          "Your Cart",
-          style: TextStyle(),
-        ),
-        Text(
-          "4 items",
-          style: Theme.of(context).textTheme.caption,
-        )
-      ],
-    ),
-  );
 }
